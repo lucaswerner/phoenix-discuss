c575741a594657bb4f53f5346598bf7e8574adb9
lucaswerner <lucaswerner26@gmail.com>
2018-10-31 23:50:55 -0300

    socket

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,16 +1,35 @@
 defmodule Discuss.CommentsChannel do
     use Discuss.Web, :channel
+    alias Discuss.{ Topic, Comment }
 
-    def join(name, _params, socket) do
-        {:ok, %{key: "there"}, socket}
+    def join("comments:" <> topic_id, _params, socket) do
+        topic_id = String.to_integer(topic_id)
+        topic = Topic
+            |> Repo.get(topic_id)
+            |> Repo.preload(comments: [:user])
+
+        {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
     end
 
-    def handle_in(name, message, socket) do
-        IO.puts("+++++++")
-        IO.puts(name)
-        IO.inspect(message)
-        IO.puts("+++++++")
+    def handle_in(name, %{"content" => content}, socket) do
+        topic = socket.assigns.topic
+        user_id = socket.assigns.user_id
 
-        {:reply, :ok, socket}
+        changeset = topic
+            |> build_assoc(:comments, user_id: user_id)
+            |> Comment.changeset(%{ content: content })
+
+        case Repo.insert(changeset) do
+            {:ok, comment} ->
+                broadcast!(
+                    socket,
+                    "comments:#{socket.assigns.topic.id}:new", 
+                    %{comment: comment}
+                )
+                {:reply, :ok, socket}
+            
+            {:error, _reason} ->
+                {:reply, {:error, %{errors: changeset}}, socket}
+        end
     end
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -5,15 +5,48 @@ let socket = new Socket("/socket", { params: { token: window.userToken } });
 
 socket.connect();
 
-let channel = socket.channel("comments:1", {});
+const createSocket = topicId => {
+  let channel = socket.channel(`comments:${topicId}`, {});
 
-channel
-  .join()
-  .receive("ok", resp => {
-    console.log("Joined successfully", resp);
-  })
-  .receive("error", resp => {
-    console.log("Unable to join", resp);
+  channel.on(`comments:${topicId}:new`, ({ comment }) =>
+    renderComment(comment)
+  );
+
+  document.querySelector("button").addEventListener("click", () => {
+    const content = document.querySelector("textarea").value;
+
+    channel.push("comment:add", { content });
   });
 
-export default socket;
+  channel
+    .join()
+    .receive("ok", resp => renderComments(resp.comments))
+    .receive("error", resp => {
+      console.log("Unable to join", resp);
+    });
+};
+
+function getCollection() {
+  return document.querySelector(".collection");
+}
+
+function commentTemplate({ content, user }) {
+  return `
+  <li class="collection-item">
+    ${content}
+    <div class="secondary-content">
+      ${user ? user.email : 'Anonymous'}
+    </div>
+  </li>
+  `;
+}
+
+function renderComments(comments) {
+  getCollection().innerHTML = comments.map(commentTemplate).join("");
+}
+
+function renderComment(comment) {
+  getCollection().innerHTML += commentTemplate(comment);
+}
+
+window.createSocket = createSocket;
